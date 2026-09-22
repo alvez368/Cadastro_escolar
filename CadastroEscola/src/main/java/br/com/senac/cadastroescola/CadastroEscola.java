df54481810bbef7a9865bd9976ac4542096df9eb
Larissa <alveslaryssa3009@gmail.com>
2026-09-22 15:20:33 -0300

    oi

---
+++ CadastroEscola/src/main/java/br/com/senac/cadastroescola/CadastroEscola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.cadastroescola;
+
+/**
+ *
+ * @author laryssa62922516
+ */
+public class CadastroEscola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
